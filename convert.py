af131a04e0b796c431fde0906fd044ec3913a521
jkowalski995 <kuba.ko.1995@gmail.com>
2022-06-12 20:27:19 +0200

    Currency Converter v0.11

--- convert.py
+++ convert.py
@@ -30,9 +30,8 @@ def calculate_one_way(code, amount):
     :return: Amount of new currency after change
     """
 
-
     ask = connect.ask_about_currency(code)[1]
-    print(ask)
+
     amount_new_curr = round(amount / ask, 2)
 
     return amount_new_curr
